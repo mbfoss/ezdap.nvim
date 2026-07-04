bfa5cc031720f15baa8bce165a3f8f8325d10c33
mbfoss <mbfoss@fastmail.com>
2026-07-04 23:31:53 +0200

    Update

--- lua/easydap/runner.lua
+++ lua/easydap/runner.lua
@@ -299,7 +299,7 @@ function M.panel_jump(n)
     local p = _panel_or_warn()
     if not p then return end
     if type(n) ~= "number" then
-        _warn("panel jump: expected a tab number, e.g. :Debug panel jump 2")
+        _warn("panel jump: expected a tab number, e.g. :Debug panel jump 2 or :2Debug panel jump")
         return
     end
     p:show_index(n)
